a0a99280e233181beaf4e73cdcf716a4ca5a90a9
Savannah Ostrowski <savannahostrowski@gmail.com>
2016-02-17 12:43:46 -0500

    added main

--- all_unique.c
+++ all_unique.c
@@ -1,8 +1,11 @@
+#include <stdbool.h>
+#include <stdio.h>
+
 bool all_unique(const char* str) {
 	int is_unique[256] = {0};
 	for (int i = 0; str[i] != '\0'; ++i) {
 		char current = str[i];
-		if (is_unique[str[i] == 0) {
+		if (is_unique[str[i]] == 0) {
 			is_unique[str[i]] = str[i];
 		} else {
 			return false;
@@ -11,3 +14,7 @@ bool all_unique(const char* str) {
 	return true;
 }
 
+int main() {
+	const char* word = "savannah";
+	printf("%d\n", all_unique(word));
+}
